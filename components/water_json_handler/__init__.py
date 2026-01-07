2fe450156e509053c96b5e4e47a4877016b74e62
htca <htca@users.noreply.github.com>
2026-01-07 13:46:13 +0100

    Implement water JSON handler with configuration schema

--- components/water_json_handler/__init__.py
+++ components/water_json_handler/__init__.py
@@ -1,7 +1,28 @@
 
+# components/water_json_handler/__init__.py
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor as sensor_comp
+from esphome.const import CONF_ID
 
-# Deze component heeft geen eigen YAML-opties; we gebruiken custom_component (lambda)
-# Een minimale CONFIG_SCHEMA is voldoende om de module te laden.
-CONFIG_SCHEMA = cg.Schema({})
+water_json_ns = cg.esphome_ns.namespace("water_json_handler")
+WaterJsonHandler = water_json_ns.class_("WaterJsonHandler", cg.Component)
+
+CONF_FLOW = "flow_sensor"
+CONF_TOTAL = "total_sensor"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(WaterJsonHandler),
+        cv.Required(CONF_FLOW): cv.use_id(sensor_comp.Sensor),
+        cv.Required(CONF_TOTAL): cv.use_id(sensor_comp.Sensor),
+    }
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    flow = await cg.get_variable(config[CONF_FLOW])
+    total = await cg.get_variable(config[CONF_TOTAL])
+    cg.add(var.set_sources(flow, total))
 ``
