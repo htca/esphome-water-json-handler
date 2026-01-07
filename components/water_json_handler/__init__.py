790bbcb7e84b486aaa9c34e49910087e4da8e217
htca <htca@users.noreply.github.com>
2026-01-07 13:39:30 +0100

    Create __init__.py for water_json_handler
    Add initial implementation of water_json_handler component

---
+++ components/water_json_handler/__init__.py
@@ -0,0 +1,7 @@
+
+import esphome.codegen as cg
+
+# Deze component heeft geen eigen YAML-opties; we gebruiken custom_component (lambda)
+# Een minimale CONFIG_SCHEMA is voldoende om de module te laden.
+CONFIG_SCHEMA = cg.Schema({})
+``
