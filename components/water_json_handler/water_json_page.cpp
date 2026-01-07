ffe76cbb85c36cf325f0aed226c01a900413366e
htca <htca@users.noreply.github.com>
2026-01-07 17:38:22 +0100

    Delete components/water_json_handler/water_json_page.cpp

--- components/water_json_handler/water_json_page.cpp
+++
@@ -1,9 +0,0 @@
-
-// components/water_json_page/water_json_page.cpp
-#include "water_json_page.h"
-namespace esphome {
-namespace water_json_page {
-// nothing here; all in header for simplicity
-}  // namespace water_json_page
-}  // namespace esphome
-``
