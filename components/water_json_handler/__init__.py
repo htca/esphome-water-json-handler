b8e76791a3803538c25a1e8168be18acc8476d0e
htca <htca@users.noreply.github.com>
2026-01-07 17:31:41 +0100

    Refactor water_json_handler for new sensor configuration

--- components/water_json_handler/__init__.py
+++ components/water_json_handler/__init__.py
@@ -1,24 +1,27 @@
 
+# components/water_json_page/__init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome import automation
 from esphome.const import CONF_ID
+from esphome.components import sensor
 
-water_json_ns = cg.esphome_ns.namespace("water_json_handler")
-WaterJsonHandler = water_json_ns.class_("WaterJsonHandler", cg.Component)
+CONF_FLOW_SENSOR = "flow_sensor"
+CONF_TOTAL_SENSOR = "total_sensor"
 
-CONF_FLOW = "flow_sensor"
-CONF_TOTAL = "total_sensor"
+ns = cg.esphome_ns.namespace("water_json_page")
+WaterJsonPage = ns.class_("WaterJsonPage", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(CONF_ID): cv.declare_id(WaterJsonHandler),
-    cv.Required(CONF_FLOW): cv.use_id(sensor.Sensor),
-    cv.Required(CONF_TOTAL): cv.use_id(sensor.Sensor),
+    cv.GenerateID(): cv.declare_id(WaterJsonPage),
+    cv.Required(CONF_FLOW_SENSOR): cv.use_id(sensor.Sensor),
+    cv.Required(CONF_TOTAL_SENSOR): cv.use_id(sensor.Sensor),
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    flow = await cg.get_variable(config[CONF_FLOW])
-    total = await cg.get_variable(config[CONF_TOTAL])
-    cg.add(var.set_sources(flow, total))
+    flow = await cg.get_variable(config[CONF_FLOW_SENSOR])
+    total = await cg.get_variable(config[CONF_TOTAL_SENSOR])
+    cg.add(var.set_flow_sensor(flow))
+    cg.add(var.set_total_sensor(total))
