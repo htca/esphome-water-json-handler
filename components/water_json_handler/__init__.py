37397da90141781f9c3b6a50691c6eaed52d8bfe
htca <htca@users.noreply.github.com>
2026-01-07 14:47:53 +0100

    Refactor water_json_handler configuration and codegen
    Updated the water_json_handler module to improve configuration schema and code generation.

--- components/water_json_handler/__init__.py
+++ components/water_json_handler/__init__.py
@@ -4,22 +4,18 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
 
-# Namespace waarin de C++ class leeft:
 water_json_ns = cg.esphome_ns.namespace("water_json_handler")
 WaterJsonHandler = water_json_ns.class_("WaterJsonHandler", cg.Component)
 
-# YAML keys voor je sensors
 CONF_FLOW = "flow_sensor"
 CONF_TOTAL = "total_sensor"
 
-# Component-config: een ID (de C++ instantie) en twee sensor-IDs
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_ID): cv.declare_id(WaterJsonHandler),
     cv.Required(CONF_FLOW): cv.use_id(sensor.Sensor),
     cv.Required(CONF_TOTAL): cv.use_id(sensor.Sensor),
 })
 
-# Codegen: instantie aanmaken, registreren en je sensors doorgeven aan set_sources()
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
