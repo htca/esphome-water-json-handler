bace3c180f7b5f396008b0ffc18ed268f93ff174
htca <htca@users.noreply.github.com>
2026-01-07 14:02:11 +0100

    Refactor water_json_handler configuration schema

--- components/water_json_handler/__init__.py
+++ components/water_json_handler/__init__.py
@@ -1,28 +1,28 @@
 
-# components/water_json_handler/__init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor as sensor_comp
+from esphome.components import sensor
 from esphome.const import CONF_ID
 
+# Namespace waarin de C++ class leeft:
 water_json_ns = cg.esphome_ns.namespace("water_json_handler")
 WaterJsonHandler = water_json_ns.class_("WaterJsonHandler", cg.Component)
 
+# YAML keys voor je sensors
 CONF_FLOW = "flow_sensor"
 CONF_TOTAL = "total_sensor"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(WaterJsonHandler),
-        cv.Required(CONF_FLOW): cv.use_id(sensor_comp.Sensor),
-        cv.Required(CONF_TOTAL): cv.use_id(sensor_comp.Sensor),
-    }
-)
+# Component-config: een ID (de C++ instantie) en twee sensor-IDs
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(CONF_ID): cv.declare_id(WaterJsonHandler),
+    cv.Required(CONF_FLOW): cv.use_id(sensor.Sensor),
+    cv.Required(CONF_TOTAL): cv.use_id(sensor.Sensor),
+})
 
+# Codegen: instantie aanmaken, registreren en je sensors doorgeven aan set_sources()
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     flow = await cg.get_variable(config[CONF_FLOW])
     total = await cg.get_variable(config[CONF_TOTAL])
     cg.add(var.set_sources(flow, total))
-``
